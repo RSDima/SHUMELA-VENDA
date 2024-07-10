a2365c6a31c69b438274f409df8e761c44de7762
sobb1e <bakimokgatle@gmail.com>
2024-07-10 11:12:20 +0200

    There are new files added and links from collaborations page have been made to each collab group.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,36 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 37,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    //Breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        800: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
